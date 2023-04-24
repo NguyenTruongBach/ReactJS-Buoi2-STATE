32b60496f6b382aa717ce9445bf0b81e6c8fe41c
Nguyen Truong Bach <bachnguyentruong2003@gmail.com>
2023-04-24 17:55:37 +0700

    BachLamNe

--- src/component/UngDungThuKinh.js
+++ src/component/UngDungThuKinh.js
@@ -18,19 +18,16 @@ class UngDungThuKinh extends Component {
     return (
       <div className="wrapper">
         <div className="header bg-dark text-white fs-1 text-center py-3">TRY GLASSES APP ONLINE</div>
-
         <div className="py-5 text-center">
           <div className="model">
             <img
               className="model-img"
               src="./glassesImage/model.jpg"
-              alt="model"
-            />
+              alt="model"/>
             <img
               className="model-glasses"
               src={this.state.Glassescuren.url}
-              alt=""
-            />
+              alt=""/>
             <div className="model-content">
               <h3>{this.state.Glassescuren.name}</h3>
               <p>{this.state.Glassescuren.desc}</p>
@@ -40,14 +37,12 @@ class UngDungThuKinh extends Component {
             <img
               className="model-img"
               src="./glassesImage/model.jpg"
-              alt="model"
-            />
-          </div>
-        </div>
+              alt="model"/>
+           </div>
+         </div>
         <div className="list-img ">{this.renderGlassesList()}</div>
       </div>
     );
   }
 }
-
   export default UngDungThuKinh;
