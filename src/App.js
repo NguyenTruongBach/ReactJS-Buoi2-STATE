e3f0b45262633ed0db054dc7a9dff9c82633b142
Nguyen Truong Bach <bachnguyentruong2003@gmail.com>
2023-04-24 17:52:16 +0700

    BachLamNe

---
+++ src/App.js
@@ -0,0 +1,12 @@
+  import React from 'react';
+  import UngDungThuKinh from './component/UngDungThuKinh';
+
+  const App = () => {
+  return (
+    <div>
+      <UngDungThuKinh/>
+    </div>
+  );
+};
+
+export default App;
